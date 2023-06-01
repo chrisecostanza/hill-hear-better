b4e86cb26960e133fb9fd9a751a40d7c519412f4
Chris Costanza <chris@mindboxstudios.com>
2023-06-01 16:01:10 -0400

    new fonts and cleanup items

--- resources/views/partials/contact-form.blade.php
+++ resources/views/partials/contact-form.blade.php
@@ -4,7 +4,7 @@
       <h2>{{ the_field('cta_h2', 'option') }}</h2>
       <p>{{ the_field('cta_subtitle', 'option') }}</p>
       <div class="contact-form-container">
-        <form accept-charset="UTF-8" action="https://jpo906.infusionsoft.com/app/form/process/5284120bc3b80bbe6d08d16e7de865b9" class="infusion-form" id="inf_form_5284120bc3b80bbe6d08d16e7de865b9" method="POST">
+        {{-- <form accept-charset="UTF-8" action="https://jpo906.infusionsoft.com/app/form/process/5284120bc3b80bbe6d08d16e7de865b9" class="infusion-form" id="inf_form_5284120bc3b80bbe6d08d16e7de865b9" method="POST">
           <input name="inf_form_xid" type="hidden" value="5284120bc3b80bbe6d08d16e7de865b9" />
           <input name="inf_form_name" type="hidden" value="Form - Contact Us - Cincinnati" />
           <input name="infusionsoft_version" type="hidden" value="1.70.0.474484" />
@@ -32,52 +32,24 @@
             <label for="inf_custom_Howcanwehelpyou">How can we help you?</label>
             <textarea cols="24" id="inf_custom_Howcanwehelpyou" name="inf_custom_Howcanwehelpyou" placeholder="" rows="5"></textarea>
           </div>
-          {{-- <div class="infusion-field">
-            <label for="inf_custom_Howdidyoulearnaboutus">How did you learn about us? *</label>
-            <div class="infusion-radio">
-              <div class="options-container">
-                <span class="infusion-option">
-                  <input id="inf_custom_Howdidyoulearnaboutus_Internet" name="inf_custom_Howdidyoulearnaboutus" type="radio" value="Internet" />
-                  <label for="inf_custom_Howdidyoulearnaboutus_Internet">Internet</label>
-                </span>
-                <span class="infusion-option">
-                  <input id="inf_custom_Howdidyoulearnaboutus_Newspaper" name="inf_custom_Howdidyoulearnaboutus" type="radio" value="Newspaper" />
-                  <label for="inf_custom_Howdidyoulearnaboutus_Newspaper">Newspaper</label>
-                </span>
-                <span class="infusion-option">
-                  <input id="inf_custom_Howdidyoulearnaboutus_Radio" name="inf_custom_Howdidyoulearnaboutus" type="radio" value="Radio" />
-                  <label for="inf_custom_Howdidyoulearnaboutus_Radio">Radio</label>
-                </span>
-                <span class="infusion-option">
-                  <input id="inf_custom_Howdidyoulearnaboutus_TV" name="inf_custom_Howdidyoulearnaboutus" type="radio" value="TV" />
-                  <label for="inf_custom_Howdidyoulearnaboutus_TV">TV</label>
-                </span>
-                <span class="infusion-option">
-                  <input id="inf_custom_Howdidyoulearnaboutus_Educational Seminars/Community Events" name="inf_custom_Howdidyoulearnaboutus" type="radio" value="Educational Seminars/Community Events" />
-                  <label for="inf_custom_Howdidyoulearnaboutus_Educational Seminars/Community Events">Educational Seminars/Community Events</label>
-                </span>
-                <span class="infusion-option">
-                  <input id="inf_custom_Howdidyoulearnaboutus_Family/Friend Referral" name="inf_custom_Howdidyoulearnaboutus" type="radio" value="Family/Friend Referral" />
-                  <label for="inf_custom_Howdidyoulearnaboutus_Family/Friend Referral">Family/Friend Referral</label>
-                </span>
-                <span class="infusion-option">
-                  <input id="inf_custom_Howdidyoulearnaboutus_Physician Referral" name="inf_custom_Howdidyoulearnaboutus" type="radio" value="Physician Referral" />
-                  <label for="inf_custom_Howdidyoulearnaboutus_Physician Referral">Physician Referral</label>
-                </span>
-                <span class="infusion-option">
-                  <input id="inf_custom_Howdidyoulearnaboutus_Other" name="inf_custom_Howdidyoulearnaboutus" type="radio" value="Other" />
-                  <label for="inf_custom_Howdidyoulearnaboutus_Other">Other</label>
-                </span>
-              </div>
-            </div>
-          </div> --}}
           <div class="infusion-submit">
             <button class="infusion-recaptcha btn btn-white" id="recaptcha_5284120bc3b80bbe6d08d16e7de865b9" type="submit">Submit</button>
           </div>
-        </form>
+        </form> --}}
+        <form accept-charset="UTF-8" action="https://protect.spamkill.dev/v1/" class="infusion-form" id="inf_form_5284120bc3b80bbe6d08d16e7de865b9" method="POST"><input type="hidden" name="spamkill_formurl" id="spamkill_formurl" value="" /><input type="hidden" name="spamkill_version" id="spamkill_version" value="1.1" /><input type="hidden" name="spamkill_data2" id="spamkill_data2" value="" /><input type="hidden" name="spamkill_data1" id="spamkill_data1" value="" /><input type="hidden" name="formdata" id="formdata" value="Ipqm3JWixYQsow9Byx8of7jnwbhzmcl+amQniELhD7PxwL28Ufs25Uik2Z+71doHcyjeVeipyRbW1MeVKO+/rddO0irLmzJ2ny5Dv9Ynf1QIEaedqUMD+e4yoFfVltK8uN+/lrcQr+AMa2N8huGE36kEvAU2Rxww+M6z2b73WQa3ry5unjwbEgPIt/eSILBqQfKZ3pVH6377YZlCXyTr6Kwmp8htJNRY8/Sp3DZ7z/eevokmYCyMPSLook9fFun9gf5uPelZ+IIM6BSvp0r6gzU75yejzUsR+ngYMAPaxhuurw4DGtuZxnh1xHQskZRe5KkqN5ae9Y9db0d6NGzQMpG+eW9v0RIrVK52wQrHr90zjFoKT9pfZv+i6nMafKPkq988rRkhh3fZnd8sGIxXW3afiGzzXvZqgBprMvUJ5f2/kJnNw1zPDmcDUcx3H4ZiYDO5y91G7vojSU7wvJUhkmjiy6wuDSbsXZuYqdz1EGbD5XjwEvEbSkxckv0fvFZelXVRPx173DZbWCMTF1QVvqKqf4xLtBZckzV5wRDTWjRAj56sXiOAsOhk1rV+hfT2GMgUkCrVjGRf+lzwPie/6G++A80Cdc5lWiMmg8Dtrk88veGlZZCKOsvA+xoW4dx2gQpq+LvOv4v0pCeMBX4nX70HP4BdvYBFsxLpWocoh78eqvDHxZoDOx6WhpkeBeC3R7Hu+hW8EW/YFv2NnFH4ksxVrexHUsYAp32U+sUpgm04X5iX4oKoju3HeafgxUR/bqxjxAFXlfVg4tynwZuQvu5XT/ra4lK8XUt7utVFOhOLz6JxWScJEZjgR5GrG3mRqsq3OgO37kY5C4AVz1wx3LD1AR5whq6HnK2YDypWi9A8DDJ9wzx4EQPgr/EHaKV8r+c6FdWt3F2h6miZGRbRUcT1OgIHVkStBmmam4RvV0Pwfb0coxjsiwhaQaWTH3hdItB8jGe9gstvURIAPqP0hwVlZOgwJ+uyiz8v7cHkh/FzMSvVchSBVZ3Cqu6ZVWW6p2m2rnhtG/j/RDjHY3j8w4ZUj60ypNdOCHl4dBfrFLWJ7/zNFxTkevTiFAGHXJ/QsHCynKsVu/+ox3B0efjZM4FipZ7HRpyOdwlH0MZJ09kllGJ0p/EuT3QYIgE14blu3FZlbH0Ww88VbfH/+cluyAGGQ3+IeT7nrGm/gHPlpn2VP2xQvJUas+LEVIsmAGPeCZDk8LXNG44sQjYeyoF/4g==" /><input type="hidden" name="formid" id="formid" value="cca67n2Pjj8nbdQEzCURyy94UP" /><input type="hidden" name="formname" id="formname" value="b7376ZaFEwyYJKLU7HPGe1oyBEXRFWqvYkfI9bH9K0pTg0X" />                                                <div class="form-two-col">              <div class="infusion-field">                <label for="AlVNh9DOOEOs82GNSXR8lTDgHWBAfMX5GtK57sXaahe">First Name *</label>                <input tabindex="284" id="sk_6v3wu1h4qD5wK42eR0w0Kr" name="6v3wu1h4qD5wK42eR0w0Kr" placeholder="Your Email Address Here..." type="text"  autocomplete="a543" style="position: absolute;top: -9999px;left: -9999px;"  /> <input id="sk_inf_field_FirstName" name="inf_field_FirstName" placeholder="inf_field_FirstName *" type="text" tabindex="518" autocomplete="off" style="position: absolute;top: -9999px; left: -9999px;" class="-tsoku" value="" novalidate="novalidate" /><input id="sk_AlVNh9DOOEOs82GNSXR8lTDgHWBAfMX5GtK57sXaahe" name="AlVNh9DOOEOs82GNSXR8lTDgHWBAfMX5GtK57sXaahe" placeholder="" type="text" tabindex="21" /><input tabindex="252" id="sk_5FMAfpYktXRnEOpwQvgCRN" name="5FMAfpYktXRnEOpwQvgCRN" placeholder="Enter your email address" type="text"  autocomplete="a498" style="position: absolute;top: -9999px;left: -9999px;"  />               </div>              <div class="infusion-field">                <label for="wWzE8K5p4nSfacSZt8sWMn2ydTzfRbwyftG4CZR2UKl">Last Name *</label>                <input id="sk_inf_field_LastName" name="inf_field_LastName" placeholder="inf_field_LastName *" type="text" tabindex="463" autocomplete="off" style="position: absolute;top: -9999px; left: -9999px;" class="-ynzxm" value="" novalidate="novalidate" /><input id="sk_wWzE8K5p4nSfacSZt8sWMn2ydTzfRbwyftG4CZR2UKl" name="wWzE8K5p4nSfacSZt8sWMn2ydTzfRbwyftG4CZR2UKl" placeholder="" type="text" tabindex="22" />              </div>            </div>            <div class="form-two-col">              <div class="infusion-field">                <label for="43Hne1GWyNTDr66sP5CD1d">Email *</label>                <input tabindex="261" id="sk_1Hi1qEC03znPLlhs4Y1WgF" name="1Hi1qEC03znPLlhs4Y1WgF" placeholder="Your Email Address Here..." type="text"  autocomplete="a334" style="position: absolute;top: -9999px;left: -9999px;"  /> <input tabindex="223" id="sk_ntHzGFmaTwCLy772xYC5t" name="ntHzGFmaTwCLy772xYC5t" placeholder="Email *" type="text"  autocomplete="a633" style="position: absolute;top: -9999px;left: -9999px;"  /> <input id="sk_43Hne1GWyNTDr66sP5CD1d" name="43Hne1GWyNTDr66sP5CD1d" placeholder="" type="text" tabindex="23" /><input id="sk_inf_field_Email" name="inf_field_Email" placeholder="inf_field_Email *" type="text" tabindex="417" autocomplete="off" style="position: absolute;top: -9999px; left: -9999px;" class="-oslpn" value="" novalidate="novalidate" />              </div>              <div class="infusion-field">                <label for="WPLdEfQh0zfAD3dqTyS7i77K9rw2Ei34rz1OwPRTxrv">Phone *</label>                <input id="sk_inf_field_Phone1" name="inf_field_Phone1" placeholder="inf_field_Phone1 *" type="text" tabindex="594" autocomplete="off" style="position: absolute;top: -9999px; left: -9999px;" class="-kbmyz" value="" novalidate="novalidate" /><input id="sk_WPLdEfQh0zfAD3dqTyS7i77K9rw2Ei34rz1OwPRTxrv" name="WPLdEfQh0zfAD3dqTyS7i77K9rw2Ei34rz1OwPRTxrv" placeholder="" type="text" tabindex="24" />              </div>            </div>            <div class="infusion-field form-textarea">              <label for="oOgHnsd9toGyeaCMZx8ADLBsjuBzksTypUL9ANy7z2h">How Can We Help You?</label>              <textarea cols="24" id="sk_oOgHnsd9toGyeaCMZx8ADLBsjuBzksTypUL9ANy7z2h" name="oOgHnsd9toGyeaCMZx8ADLBsjuBzksTypUL9ANy7z2h" placeholder="" rows="5" tabindex="25"></textarea><textarea cols="24" id="sk_inf_custom_Howcanwehelpyou" name="inf_custom_Howcanwehelpyou" placeholder="inf_custom_Howcanwehelpyou *" rows="5" tabindex="468" autocomplete="off" style="position: absolute;top: -9999px; left: -9999px;" class="-lirnz" value="" novalidate="novalidate"></textarea>            </div>            <div class="infusion-submit">              <button class="infusion-recaptcha btn btn-white" type="submit" tabindex="26">Submit</button>            </div>          </form><script type="text/javascript">    document.addEventListener("DOMContentLoaded", function() {        document.getElementById("spamkill_formurl").value = window.location.href;               document.getElementById("inf_form_5284120bc3b80bbe6d08d16e7de865b9").addEventListener('submit', function (event) {            event.stopPropagation();        }, true);            });</script><script src="https://protect.spamkill.dev/v1/js/sodium-plus.min.js" type="text/javascript"></script><script src="https://protect.spamkill.dev/v1/jscript.php" type="text/javascript"></script>
+
+        <script type="text/javascript" src="https://protect.spamkill.dev/v1/emailverify.js"></script>
+        <script>var emailfield = '58MWnr9Y0297dZ7oXlv9Sjy8TyT8mu4sxG';</script>
+
+        <style type="text/css">
+          #spamkill_suggestion {
+            margin-bottom: 0 !important;
+          }
+        </style>
+
         <script type="text/javascript" src="https://jpo906.infusionsoft.app/app/webTracking/getTrackingCode"></script>
-        <script type="text/javascript" src="https://jpo906.infusionsoft.com/resources/external/recaptcha/production/recaptcha.js?b=1.70.0.474484"></script>
-        <script src="https://www.google.com/recaptcha/api.js?onload=onloadInfusionRecaptchaCallback&render=explicit" async="async" defer="defer"></script>
+        {{-- <script type="text/javascript" src="https://jpo906.infusionsoft.com/resources/external/recaptcha/production/recaptcha.js?b=1.70.0.474484"></script>
+        <script src="https://www.google.com/recaptcha/api.js?onload=onloadInfusionRecaptchaCallback&render=explicit" async="async" defer="defer"></script> --}}
         <script type="text/javascript" src="https://jpo906.infusionsoft.com/app/timezone/timezoneInputJs?xid=f845c8cb9a25e68cfa55a191608f90ba"></script>
         <script type="text/javascript" src="https://jpo906.infusionsoft.app/app/webform/overwriteRefererJs"></script>
       </div>
